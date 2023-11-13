e5b1c2d90bafc966e9bb3efe9586e3ce100bc2c3
AMBE Hideki <ykc@daycrift.net>
2023-11-14 02:26:06 +0900

    feat: switch db by ENV 'DB_TYPE'
    it also requires mysql, postgresql in local env

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 # Activate the gem you are reporting the issue against.
 gem "activerecord", "~> 7.1.0"
 gem "sqlite3"
+
+gem "mysql2"
+
+gem "postgresql"
--- app/database_setup.rb
+++ app/database_setup.rb
@@ -1,9 +1,25 @@
 require 'active_record'
+require 'yaml'
 
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: './sql.db',
-)
+case db_type = ENV.fetch('DB_TYPE', 'sqlite3')
+when 'mysql', 'postgresql'
+  config = YAML.load_file(File.join(__dir__, "config", "#{db_type}.yml"))
+  ActiveRecord::Base.establish_connection(
+    adapter: config['adapter'],
+    host: config['host'],
+    port: config['port'],
+    database: config['database'],
+    username: config['username'],
+    password: config['password']
+  )
+else
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: './sql.db',
+  )
+end
+
+
 
 Dir[File.join(__dir__, 'models', '**')].each do |file|
   require file
